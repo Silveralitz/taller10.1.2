6e432f6cd0bfb5feaa2423f31ef4460ed2b34995
Mati.Silvera <www.matias0902@gmail.com>
2025-09-05 12:51:37 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,45 @@
+// Array extraño
+const strangeArray = [
+  "Zero",
+  function () {
+    alert("Hola soy una función en un array");
+  },
+  22,
+  null,
+  "Go lang",
+  undefined,
+  "Cobol",
+  "I'm programmer",
+  -2000,
+  "Hello world",
+  `One is ${1}`,
+  { name: "Info", lastname: "last info" },
+  () => true,
+  function showNumbers() {
+    return "1, 2, 3, 4";
+  },
+  "Another String",
+  ["Hola mundo!"],
+  "b is a letter",
+  "JavaScript",
+];
+
+// Función que recibe por parámetro un array y muestra sus elementos en pantalla
+function showList(array) {
+  const container = document.getElementById("list");
+  container.innerHTML = "";
+  array.forEach((element) => {
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(element));
+    container.appendChild(li);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  // Filtrar solo los strings
+  const onlyStrings = strangeArray.filter(item => typeof item === "string");
+  // Ordenar alfabéticamente ignorando mayúsculas/minúsculas
+  onlyStrings.sort((a, b) => a.localeCompare(b, 'es', { sensitivity: 'base' }));
+  // Mostrar en pantalla
+  showList(onlyStrings);
+});
